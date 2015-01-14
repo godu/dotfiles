8f496a0487885edc970be84cb97fc6eb4a9a951c
godu <arthur.weber@viacesi.fr>
2015-01-14 12:50:08 +0100

    Adds execution permission & Cleanup after install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,9 +13,13 @@ brew update
 brew upgrade
 
 # Install package
-brew install git mongodb node pandoc wget unrar brew-cak nginx
+brew install brew-cask git mongodb node pandoc wget unrar nginx
 # Install software
 brew cask install atom firefox github launchrocket robomongo onyx vagrant vlc chromium gimp iterm2 virtualbox
+
+# Cleanup
+brew cleanup
+brew cask cleanup
 
 # Audit installation
 brew doctor
