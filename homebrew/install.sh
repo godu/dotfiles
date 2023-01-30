c06bf7178af05773b54c30fe1af57fc2ac7d528c
Arthur Weber <arthur.weber@viacesi.fr>
2023-01-30 13:21:56 +0100

    Update install.sh

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,15 +1,18 @@
 ##!/bin/sh
-#
-# Homebrew
-#
-open https://brew.sh/
+
+# Homebrew - https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Update
 brew update --all
 brew upgrade
 
 # Install
-brew install iterm2 visual-studio-code slack firefox google-chrome chromium spotify spectacle
+brew install ghcup
+brew install --cask authy chromium docker firefox google-chrome iterm2 slack spectacle spotify visual-studio-code
+
+# Node Manager - https://github.com/Schniz/fnm
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "./.fnm" --skip-shell
 
 # Cleanup
 brew cleanup
